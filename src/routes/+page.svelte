980d2e33e12e173802242783748b5b322172779d
Harry Taylor <harry.taylor.dev.uk@gmail.com>
2024-02-15 23:34:21 +0000

    adding files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { goto } from "$app/navigation";
+
+    const handleclick = () => {
+        goto('/create');
+    }
+</script>
+
+<div class="button-container">
+    <button class="date_button">Countdowns</button>
+    <button class="sort_button">Sort</button>
+</div>
+
+<div class="list_box">
+
+</div>
+
+
+<div class="add_container">
+    <button on:click={handleclick} class="add_item_button">
+        <img class="plus" src="/plus.png" alt="plus" />
+    </button>
+</div>
+
+
